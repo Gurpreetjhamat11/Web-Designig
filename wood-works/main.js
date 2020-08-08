0097c1462a09e3a30842ac2f3b8e7389afc35731
Gurpreetjhamat11 <57304112+Gurpreetjhamat11@users.noreply.github.com>
2020-08-08 00:05:17 -0400

    mid term

---
+++ wood-works/main.js
@@ -0,0 +1,9 @@
+// alert ("If you like the code please upvote it!")
+function openSlideMenu(){
+document.getElementById("side-menu").style.width="250px";             
+          
+}
+
+function closeSlideMenu(){
+          document.getElementById("side-menu").style.width="0px";}
+          
