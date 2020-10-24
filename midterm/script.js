8fe8430b3df972a497dec0521afb7bc31cdc5ede
Gurpreetjhamat11 <57304112+Gurpreetjhamat11@users.noreply.github.com>
2020-10-24 00:21:54 -0400

    mid term uploaded

---
+++ midterm/script.js
@@ -0,0 +1,31 @@
+$("#form").submit(function(e){
+    e.preventDefault();
+});
+
+function submitData() {
+    let firstName = $("#firstName").val();
+    let lastName = $("#lastName").val();
+    let phone = $("#phone").val();
+    let studentId = $("#studentId").val();
+    let address = $("#address").val();
+    let age = $("#age").val();
+    let vehicle = $(".radio:checked").val();
+    let gender = $( "#gender option:selected" ).val();
+    let life = $( "#life option:selected" ).val();
+    $("#displayArea").empty();
+    appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life);
+}
+// console.log($("displayArea"))
+function appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life){
+    $("#displayArea").append(`<li>First Name: ${firstName}</li>`);
+    $("#displayArea").append(`<li>Last Name: ${lastName}</li>`);
+    $("#displayArea").append(`<li>Phone Number: ${phone}</li>`);
+    $("#displayArea").append(`<li>Student Id: ${studentId}</li>`);
+    $("#displayArea").append(`<li>Address: ${address}</li>`);
+    $("#displayArea").append(`<li>Age: ${age}</li>`);
+    $("#displayArea").append(`<li>Own a car: ${vehicle}</li>`);
+    $("#displayArea").append(`<li>Gender: ${gender}</li>`);
+    $("#displayArea").append(`<li>Martial Status: ${life}</li>`);
+    // console.log('appendData')
+    // console.log(firstName, lastName, phone, studentId, address, age, vehicle, gender, life)
+}    
