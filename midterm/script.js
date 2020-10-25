38eec436c1ddfac7514e6b926a1d0a859cda09df
Gurpreetjhamat11 <57304112+Gurpreetjhamat11@users.noreply.github.com>
2020-10-25 12:48:33 -0400

    Update script.js

--- midterm/script.js
+++ midterm/script.js
@@ -12,11 +12,12 @@ function submitData() {
     let vehicle = $(".radio:checked").val();
     let gender = $( "#gender option:selected" ).val();
     let life = $( "#life option:selected" ).val();
+    let group = $("#bloodGroup option:selected")
     $("#displayArea").empty();
-    appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life);
+    appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life, group);
 }
 // console.log($("displayArea"))
-function appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life){
+function appendData(firstName, lastName, phone, studentId, address, age, vehicle, gender, life, group){
     $("#displayArea").append(`<li>First Name: ${firstName}</li>`);
     $("#displayArea").append(`<li>Last Name: ${lastName}</li>`);
     $("#displayArea").append(`<li>Phone Number: ${phone}</li>`);
@@ -26,6 +27,7 @@ function appendData(firstName, lastName, phone, studentId, address, age, vehicle
     $("#displayArea").append(`<li>Own a car: ${vehicle}</li>`);
     $("#displayArea").append(`<li>Gender: ${gender}</li>`);
     $("#displayArea").append(`<li>Martial Status: ${life}</li>`);
+    $("#displayArea").append(`<li>Blood Group: ${group}</li>`);
     // console.log('appendData')
     // console.log(firstName, lastName, phone, studentId, address, age, vehicle, gender, life)
 }    
